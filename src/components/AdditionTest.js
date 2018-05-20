450f2984a10277cfca053cfb0447842306125ef1
Ivan Kovic <ivan.kovic@seekandhit.com>
2018-05-20 15:12:39 +0200

    Naive timer solution

--- src/components/AdditionTest.js
+++ src/components/AdditionTest.js
@@ -12,8 +12,10 @@ class AdditionTest extends PureComponent {
     x: 0,
     y: 0,
     input: '',
-    error: false,
     counter: 0,
+    lives: 3,
+    timerId: null,
+    timerValue: 10,
   };
 
   inputRef = React.createRef();
@@ -39,6 +41,8 @@ class AdditionTest extends PureComponent {
     const x = getRandomInt(1, 19);
     const y = getRandomInt(1, 20 - x);
 
+    this.startTimer();
+
     this.setState(
       state => ({
         x,
@@ -55,28 +59,61 @@ class AdditionTest extends PureComponent {
     this.setState({ input: e.target.value });
   };
 
+  handleMove = () => {
+    const { lives } = this.state;
+
+    switch (lives) {
+      case 0:
+        return alert('Izgubila si! :(');
+      case 10:
+        return alert('Bravo! Pobijedila si! :)');
+      default:
+        return this.generateAddends();
+    }
+  };
+
   handleSubmit = () => {
     const { x, y, input } = this.state;
 
     if (x + y !== Number(input)) {
-      return this.setState({ error: true }, this.focusInput);
+      return this.setState(state => ({ lives: state.lives - 1 }), this.handleMove);
+    } else {
+      return this.setState(state => ({ lives: state.lives + 1 }), this.handleMove);
+    }
+  };
+
+  startTimer = () => {
+    const { timerId } = this.state;
+    if (timerId) {
+      clearInterval(timerId);
     }
 
-    this.generateAddends();
+    const newTimerId = window.setInterval(() => {
+      const { timerId, timerValue } = this.state;
+      const newTimerValue = timerValue - 1;
+
+      if (newTimerValue === 0) {
+        clearInterval(timerId);
+        return this.handleSubmit();
+      }
+
+      this.setState({ timerValue: newTimerValue });
+    }, 1000);
+    this.setState({ timerId: newTimerId, timerValue: 10 });
   };
 
   render() {
-    const { x, y, input, error, counter } = this.state;
+    const { x, y, input, counter, timerValue, lives } = this.state;
     return (
       <div className="test">
         <div className="hearts">
-          {R.range(0, counter - 1).map(idx => <img key={idx} className="heart" alt="heart" src={heart} />)}
+          {R.range(0, lives).map(idx => <img key={idx} className="heart" alt="heart" src={heart} />)}
         </div>
         <span className="calculator">
           <b>{counter}:</b> {x} + {y} ={' '}
           <input type="number" onChange={this.handleInputChange} value={input} ref={this.inputRef} />
-          {error && <span className="error">Krivo!</span>}
         </span>
+        <div>{timerValue}</div>
       </div>
     );
   }
